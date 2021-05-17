8fd48407d4bfabcb0de9b15ed1bff8fbfe9d2855
gicunho <gicunho@hotmail.it>
2021-05-17 16:01:35 +0200

    Aggiunto BONUS

---
+++ Bonus/assets/js/main.js
@@ -0,0 +1,10 @@
+//Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const app = new Vue({
+    el: '#root',
+    data: {
+        titolo: "Assaggio di Vue JS",
+        sfondoVJS: "background-image: url('https://xpertlab.com/wp-content/uploads/2020/10/1_wFL3csJ96lQpY0IVT9SE3w.jpeg'); height: 100vh; object-fit: contain; background-repeat: no-repeat;"
+    }
+})
+
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
