494ae21e2bd651ffb536386808ef8cf5ed1d8af7
gicunho <gicunho@hotmail.it>
2021-05-17 15:36:07 +0200

    Aggiunto Titolo H1 con data di Vue

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const app = new Vue({
+    el: '#root',
+    data: {
+        titolo: "Assaggio di Vue JS"
+    }
+})
+
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
